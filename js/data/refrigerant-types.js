c2d86a6ad55cb618f8b88ab2afbdb2340b2eeb0a
mmcdani2 <markalexandermcdaniel@gmail.com>
2026-03-06 18:08:27 -0600

    shipping mvp v1

--- js/data/refrigerant-types.js
+++ js/data/refrigerant-types.js
@@ -1,13 +1,15 @@
 export const REFRIGERANT_TYPES = [
-  { value: 'R-22', label: 'R-22' },
   { value: 'R-410A', label: 'R-410A' },
-  { value: 'R-32', label: 'R-32' },
+  { value: 'R-22', label: 'R-22' },
   { value: 'R-454B', label: 'R-454B' },
+  { value: 'R-32', label: 'R-32' },
   { value: 'R-134a', label: 'R-134a' },
   { value: 'R-404A', label: 'R-404A' },
   { value: 'R-407C', label: 'R-407C' },
   { value: 'R-407A', label: 'R-407A' },
   { value: 'R-448A', label: 'R-448A' },
   { value: 'R-449A', label: 'R-449A' },
+  { value: 'R-438A', label: 'R-438A (MO99)' },
+  { value: 'R-422D', label: 'R-422D' },
   { value: 'Other', label: 'Other' }
 ]
